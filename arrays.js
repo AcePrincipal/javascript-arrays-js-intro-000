8f2224235ec8f34c02dfca8e6aa70969e190b49c
Alexander Petrov <acepetrov96@gmail.com>
2020-09-05 21:43:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,3 +27,7 @@ function destructivelyRemoveElementFromBeginningOfArray(arr) {
 function removeElementFromBeginningOfArray(arr) {
   return arr.slice(1);
 }
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  arr.pop();
+  return arr;
+}
