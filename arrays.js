196874904ee0895f60d04144dd0fa31d8b37f2d1
Alexander Petrov <acepetrov96@gmail.com>
2020-09-05 21:03:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,11 @@ function destructivelyAddElementToBeginningOfArray(arr, e) {
   arr.unshift(e);
   return arr;
 }
+function addElementToEndOfArray(arr, e){
+  arr1 = [e, ...arr];
+  return arr1;
+}
+function destructivelyAddElementToEndOfArray(arr, e) {
+  arr.unshift(e);
+  return arr;
+}
