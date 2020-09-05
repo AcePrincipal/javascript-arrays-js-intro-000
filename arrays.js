a311f76472a5ba08abe04556b098ea989b25ff8a
Alexander Petrov <acepetrov96@gmail.com>
2020-09-05 20:57:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(arr, e){
+  return unshift(arr, e);
+}
+function destructivelyAddElementToBeginningOfArray(arr, e) {
+  arr[0] = e;
+  return arr;
+}
